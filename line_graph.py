1c0fc1d18e5b984da4674aa9f7530857d29e75d0
ccn <ccn@cuppajoeman.com>
2021-07-26 19:50:57 +0000

    create melody sheet

--- line_graph.py
+++ line_graph.py
@@ -24,7 +24,7 @@ def create_line_graph(x_values, y_values, x_start, x_end, width, height ):
         x_values = x_values,
         y_values = y_values,
         line_color=GOLD_E,
-        vertex_dot_style=dict(stroke_width=3,  fill_color=PURPLE),
+        vertex_dot_style=dict(stroke_width=5,  fill_color=PURPLE),
         stroke_width = 5,
     )
     labels = VGroup()
