c89c532a0d797db90fd166e7a3f5debaddd216ed
ccn <ccn@cuppajoeman.com>
2021-07-29 00:58:55 +0000

    create melody sheet

--- line_graph.py
+++ line_graph.py
@@ -3,6 +3,7 @@ from numuse import  converters
 import numuse.music
 from fractions import Fraction
 import math
+import graph_music_data
 
 
 SCALE = 12
@@ -91,52 +92,10 @@ class LineGraphExample(Scene):
         height=SCALE * math.sqrt(2)
 
 
-        b = 1
-        # half
-        h = 1 / 2
-        # thirds
-        t = Fraction(b, 3)
-        # two thirds
-        tt = 2 * t
-
-        jens_solo_arps = [
-            [("0' 4' 7' 11' R", [tt, t, tt, t + b, b])],
-            [("R 7' 4' 7' 7'", [tt, t, tt, t + b, b])],
-            [("9' 6' R", [b + tt, t + b, b])],
-            [("6' 2' 9' 6' 0'' 9' R", [tt, t, tt, t, tt, t, b])],
-            [("5' 2' 9' 0'' 5'", [tt, t, tt, t + b, b])],
-            [("2' 7 11 2' 5' 2' R", [tt, t, tt, t, tt, t, b])],
-            [("4' 4' 4' 0'", [b, tt, t + b, b])],
-            [("11 7 11 2' 5' 2' R", [tt, t, tt, t, tt, t, b])],
-            [("4' 0' R 4' R 4' R 4'", [tt, t, tt, t, tt, t, tt, t])],
-            [("7' 4' 7' 7' R 11'", [tt, t, tt, t, 1 + tt, t])],
-            [("0'' 0'' 9' R 9' 6'", [b, tt, t, tt, t, b])],
-            [("R 2' 2' 2' R", [b, b, tt, t, b])],
-            [("0'' 0'' 9' R 9' 5'", [b, tt, t, tt, t, b])],
-            [("R 2' 11 5' 2' 11 7", [b, tt, t, tt, t, tt, t])],
-            [("4' 0' R 4' 0' R", [tt, t, tt, t, b, b])],
-            [("7' 4' 7' 10' R", [tt, t, tt, t + b, b])],
-            [("R 9 0' 9 0' 9", [b, b, tt, t, tt, t])],
-            [("5 9 0' 4' R", [tt, t, tt, t + b, b])],
-            [("R 0' 4' 0' R", [b, b, tt, t, b])],
-            [("9 0' 5 R", [b, tt, t + b, b])],
-            [("2' R 0'' 6' 9' 6'", [b, b, tt, t, tt, t])],
-            [("0'' 0'' 9' R", [b, tt, t + b, b])],
-            [("9' R 9' 2' 5' 2'", [b, b, tt, t, tt, t])],
-            [("11 11 2' R", [b, tt, t + b, b])],
-            [("4' 0' 7' 0' R 4' 7' 11'", [tt, t, tt, t, tt, t, tt, t + b])],
-            [("7' 11' R", [tt, t + b, b])],
-            [("6' 2' 9' 2' R 9'", [tt, t, tt, t, b, b])],
-            [("0'' 6' R 9' R", [tt, t, tt, t + b, b])],
-            [("9' 2' R 5' 2'", [tt, t, tt, t + b, b])],
-            [("5' 11 R 2' 5'", [tt, t, tt, t + b, b])],
-            [("4' 0' 7' 4' 7' 11'", [tt, t, tt, t, tt, t + b])],
-            [("R", [4 * b])],
-        ]
 
         all_graphs = VGroup()
         points = []
-        measures = converters.parse_music_measures(jens_solo_arps)
+        measures = converters.parse_music_measures(graph_music_data.take_the_a_train)
         num_lines = len(measures) // 4
         graph_height = height / num_lines
         m = numuse.music.Music(measures, 120)
